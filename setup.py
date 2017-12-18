c96a24cda027fa526ad48bd7a37fa0e15a706980
Enrique Sanjuanelo <enrique.jose.sanjuanelo.robles@philips.com>
2017-12-18 21:38:04 +0100

    CI Tox

--- setup.py
+++ setup.py
@@ -5,26 +5,13 @@ flask extension that provides an interface to eureka via a flask.app
 """
 
 
-import os.path
-
 from setuptools import setup, find_packages
-
-LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-# Get requirements
-requirements = []
-with open(os.path.join(LOCAL_DIR, 'requirements.txt'), 'r') as infile:
-    for line in infile:
-        line = line.strip()
-        if line and not line[0] == '#':  # ignore comments
-            requirements.append(line)
 
 setup(
     name='flask-eureka',
     packages=find_packages(exclude=['tests*','examples*']),
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=['Flask'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
