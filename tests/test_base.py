39ed1354f485f2ca9978b552f58ed30374b1cc61
Enrique Sanjuanelo <enrique.jose.sanjuanelo.robles@philips.com>
2017-12-18 14:18:51 +0100

    Test CI

---
+++ tests/test_base.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestBase(unittest.TestCase):
+    """
+    Tests the base functionality
+    """
+    def test_simple(self):
+        """
+        Simple test
+        """
+        self.assertTrue(True)
